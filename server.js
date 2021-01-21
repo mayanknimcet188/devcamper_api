ec329239abdef73d5e35d7e96d433c785ae8e320
mayanknimcet188 <205119053@nitt.edu>
2021-01-21 23:24:55 +0530

    Added basic CRUD operations for the bootcamp routes

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 const express = require('express');
 const dotenv = require('dotenv');
-//Route files
-const bootcamps = require('./routes/bootcamps');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+
 //Load env vars
 dotenv.config({path: './config/config.env'});
 
+//Connect DB
+connectDB();
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
 
 //Mount routers
 app.use('/api/v1/bootcamps',bootcamps)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=> console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)); 
+const server = app.listen(PORT,()=> console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)); 
+
+// Handle unhandled promise rejections (due to DB connection error etc )
+process.on('unhandledRejection',(err,promise)=> {
+    console.error(`Error: ${err.message}`.red)
+    //Close the connection to the server
+    server.close(()=> process.exit(1));
+});
